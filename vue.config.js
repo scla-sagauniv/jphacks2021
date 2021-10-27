d9d6a215821231f2285a3b5c4d38893beb84b671
Kai <kaiisiduka@gmail.com>
2021-10-28 00:14:07 +0900

    add public

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ module.exports = {
   baseUrl: './',
   devServer: {
     // host: news-typing-scla.herokuapp.com,
-    disableHostCheck: true
+    public: 'news-typing-scla.herokuapp.com:8080',
+    disableHostCheck: true,
   }
 }
