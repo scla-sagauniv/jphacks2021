f379a11ec7f56a7995623ea0371a67d285e82081
shunsuke-tamura <wwwxx4869@gmail.com>
2021-10-28 23:19:24 +0900

    update: 問題のinputStringを問題文に対応させた

--- src/getNews.js
+++ src/getNews.js
@@ -1,25 +1,16 @@
 import toHiragana from "./toHiragana"
 import toRome from "./toRome"
 async function getNews(pageNumber, categorySelect){
-  let mondai_list = [];
+  let mondai_list = [], for_input;
   const API_KEY = "";
   const url = `https://newsapi.org/v2/top-headlines?pageSize=${pageNumber}&category=${categorySelect}&country=jp&apiKey=${API_KEY}`;
   let res = await fetch(url);
   let json = await res.json();
   for(let i = 0; i < json.articles.length; i++){
-    mondai_list.push({displayString: json.articles[i].title, inputString: "aaaaaaa"});
+    res = await toHiragana(json.articles[i].title)
+    for_input = await toRome(res, 'kunrei')
+    mondai_list.push({displayString: json.articles[i].title, inputString: for_input});
   }
-  // // デバッグ用
-  // toHiragana("「Todayの2.4限目の量子力学入門Ｔｅｓｔ、てげよだきっちゃけど。」PTAに訴えてもらおうぜ")
-  // // toHiragana("3月1日は日曜日で祝日、晴れの日でした")
-  // .then((res) => {
-  //   console.log("「Todayの2.4限目の量子力学入門Ｔｅｓｔ、てげよだきっちゃけど。」PTAに訴えてもらおうぜ")
-  //   // console.log("3月1日は日曜日で祝日、晴れの日でした");
-  //   console.log(res);
-  //   console.log(toRome(res, 'kunrei'));
-  // })
-  // // let test = "「Todayの2.4げんめのりょうしりきがくにゅうもんＴｅｓｔ、てげよだきっちゃけど。」PTAにうったえてもらおうぜ";
-  // // console.log(toRome(test, 'kunrei'));
   return mondai_list
 }
 export default getNews
