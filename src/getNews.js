cd3aff118033d8f3a21c786ec107fe299592b7ed
shunsuke.tamura <wwwxx4869@gmail.com>
2021-10-28 19:34:29 +0900

    update: デバッグ用のプログラムを追記

--- src/getNews.js
+++ src/getNews.js
@@ -1,3 +1,5 @@
+import toHiragana from "./toHiragana"
+
 async function getNews(){
   let mondai_list = [];
   const API_KEY = "";
@@ -7,6 +9,13 @@ async function getNews(){
   for(let i = 0; i < json.articles.length; i++){
     mondai_list.push({displayString: json.articles[i].title, inputString: "aaaaaaa"});
   }
+  // toHiragana("「Todayの2.4限目の量子力学入門Ｔｅｓｔ、てげよだきっちゃけど。」PTAに訴えてもらおうぜ")
+  toHiragana("3月1日は日曜日で祝日、晴れの日でした")
+  .then((res) => {
+    console.log("3月1日は日曜日で祝日、晴れの日でした");
+    console.log(res);
+  })
+
   return mondai_list
 }
 export default getNews
