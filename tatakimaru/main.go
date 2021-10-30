a76cc465c0412dd3277edb231e1cbdb511df2ec5
shunsuke-tamura <wwwxx4869@gmail.com>
2021-10-30 13:50:16 +0900

    update: apikeyを新たに取得

--- tatakimaru/main.go
+++ tatakimaru/main.go
@@ -54,7 +54,7 @@ type resNews struct {
 func getNews(ctx *gin.Context) {
 	pages := ctx.Query("pageSize")
 	cat := ctx.Query("category")
-	API_KEY := "a301759d867c4aeebf416dff3af6dfad"
+	API_KEY := "c49f779b1bad4a778e96648d34e323ab"
 	url := "https://newsapi.org/v2/top-headlines?pageSize="+ pages + "&category=" + cat + "&country=jp&apiKey=" + API_KEY
 	
 	res, err := http.Get(url)
