55a4c1309750f00a7550818dc8d94d43dffa3f4a
shunsuke-tamura <wwwxx4869@gmail.com>
2021-11-18 15:34:16 +0900

    rename: 関数名を変更

--- src/addRuby.js
+++ src/addRuby.js
@@ -1,20 +1,21 @@
-async function makeRuby(mondai_list) {
+async function addRuby(mondai_list) {
     const allSent = mergeSent(mondai_list)
     const json = await postFurigana(allSent)
     console.log(json)
-    let ruby_list = [];
+    let ruby_list = [], ruby_list_part = [];
     for (let i = 0; i < json.result.word.length; i++) {
         if (json.result.word[i].subword) {
             for (let n = 0; n < json.result.word[i].subword.length; n++) {
                 if (/\p{sc=Han}/u.test(json.result.word[i].subword[n].surface)) {
-                ruby_list.push({ kanzi: json.result.word[i].subword[n].surface, furigana: json.result.word[i].subword[n].furigana })
+                    ruby_list_part.push({ kanzi: json.result.word[i].subword[n].surface, furigana: json.result.word[i].subword[n].furigana })
                 }
             }
         }
-        else {
-            if (/\p{sc=Han}/u.test(json.result.word[i].surface)) {
-                ruby_list.push({ kanzi: json.result.word[i].surface, furigana: json.result.word[i].furigana })
-            }
+        else if (/\p{sc=Han}/u.test(json.result.word[i].surface)) {
+            ruby_list_part.push({ kanzi: json.result.word[i].surface, furigana: json.result.word[i].furigana })
+        }
+        else if (json.result.word[i].surface === "<" && json.result.word[i+1].surface === ">") {
+            ruby_list.push(ruby_list_part)
         }
     }
     return ruby_list
@@ -36,4 +37,4 @@ function mergeSent(mondai_list) {
     return sent_list.join('<>')
 }
 
-export default makeRuby
+export default addRuby
