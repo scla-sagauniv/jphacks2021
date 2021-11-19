028e8691c544d4e7af609fb9239c06a96d831f84
dorapon48 <dorapon48@icloud.com>
2021-11-19 23:53:33 +0900

    twitterアイコンの追加，シェア機能の表示の変更

--- src/components/GameEndDisplay.vue
+++ src/components/GameEndDisplay.vue
@@ -50,10 +50,13 @@
         <br>
         <b-button variant="primary" @click="gameStart">Retry</b-button>
         <br>
-        <b-button variant="primary" @click="shareTwitter">Twitter共有</b-button>
+        <a v-bind:href="href_title" target="_blank" rel="noopener noreferrer" @click="shareTwitter" class="twitter-share-button" data-show-count="false">
+        <img src="../assets/Twitter social icons - rounded square - blue.svg">
+        </a>
     </div>
 </template>
 
+<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 <script>
   import getNews from '@/getNews'
   export default {
@@ -103,6 +106,7 @@
           missSubmit: this.$store.state.missEnter,
           totalTime: Math.floor((this.$store.state.endTime - this.$store.state.startTime) / 100) / 10, // ミリ秒なので小数第一位までの秒に変換して
           resultScore: 0,
+          href_title: "test",
         }
     },
     mounted() {
@@ -127,7 +131,8 @@
         text += this.create_score_text(this.resultScore) + "\n"
         text += "#N_Typing #SCLA"
         let twitter_share_url = "https://twitter.com/intent/tweet?" + "text=" + encodeURIComponent(text);
-        window.open(twitter_share_url, '_blank')
+        this.href_title = twitter_share_url;
+        // window.open(twitter_share_url, '_blank')
       },
       toNews(url){
         window.open(url, '_blank')
@@ -190,6 +195,11 @@
       font-size: 27px;
       margin: 5px 0px;
     }
+    img {
+      margin-top: 20px;
+      width: 5%;
+      height: 5%;
+    }
     .score-titles {
         position: absolute;
         left: 0;
@@ -201,4 +211,24 @@
     .-result {
         color: red
     }
+    // .twitter-share-button{
+    //   position: relative;
+    //   height: 20px;
+    //   box-sizing: border-box;
+    //   padding: 1px 12px 1px 12px;
+    //   background-color: #1d9bf0;
+    //   color: white;
+    //   border-radius: 1000px;
+    //   font-weight: 500;
+    //   cursor: pointer;
+    // }
+    // .twitter-share-button i{
+    //   position: relative;
+    // top: 2px;
+    // display: inline-block;
+    // width: 14px;
+    // height: 14px;
+    // background: transparent 0 0 no-repeat;
+    // background-image: url(data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%2072%2072%22%3E%3Cpath%20fill%3D%22none%22%20d%3D%22M0%200h72v72H0z%22%2F%3E%3Cpath%20class%3D%22icon%22%20fill%3D%22%23fff%22%20d%3D%22M68.812%2015.14c-2.348%201.04-4.87%201.744-7.52%202.06%202.704-1.62%204.78-4.186%205.757-7.243-2.53%201.5-5.33%202.592-8.314%203.176C56.35%2010.59%2052.948%209%2049.182%209c-7.23%200-13.092%205.86-13.092%2013.093%200%201.026.118%202.02.338%202.98C25.543%2024.527%2015.9%2019.318%209.44%2011.396c-1.125%201.936-1.77%204.184-1.77%206.58%200%204.543%202.312%208.552%205.824%2010.9-2.146-.07-4.165-.658-5.93-1.64-.002.056-.002.11-.002.163%200%206.345%204.513%2011.638%2010.504%2012.84-1.1.298-2.256.457-3.45.457-.845%200-1.666-.078-2.464-.23%201.667%205.2%206.5%208.985%2012.23%209.09-4.482%203.51-10.13%205.605-16.26%205.605-1.055%200-2.096-.06-3.122-.184%205.794%203.717%2012.676%205.882%2020.067%205.882%2024.083%200%2037.25-19.95%2037.25-37.25%200-.565-.013-1.133-.038-1.693%202.558-1.847%204.778-4.15%206.532-6.774z%22%2F%3E%3C%2Fsvg%3E);
+    // }
 </style>
